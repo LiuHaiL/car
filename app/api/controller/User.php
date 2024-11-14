8719302d5d6d0aa0e79fddff92782392456d20ca
liu <18655112670@139.com>
2024-11-14 17:38:36 +0800

    123456

--- app/api/controller/User.php
+++ app/api/controller/User.php
@@ -106,7 +106,7 @@ class User extends Frontend
     }
 
 
-    //用户实名认证
+    //用户实名认证1212211221
     public function userRealNameAuth() : void {
         if ($this->request->isPost()) {
             $data   = $this->request->param();
